8710c97a07d5a42ded169df69cee8681cfd6bbb7
rdteixeira <rodrigo@computatus.org>
2023-08-19 14:16:28 -0300

    Alterações - Callback/Asaas - Reorganização de Codigo

--- public_html/callback/asaas/notification.php
+++ public_html/callback/asaas/notification.php
@@ -1,15 +1,10 @@
 <?php
 
-$server = (object) $_SERVER;
-$contents = "$server->REQUEST_METHOD $server->REQUEST_URI $server->SERVER_PROTOCOL\n";
-foreach(getallheaders() as $key => $value) $contents .= "$key: $value\n";
-$payload = file_get_contents('php://input');
-$contents .= "\n".$payload;
-list($sec, $u_sec) = explode('.', microtime(true));
-file_put_contents('request-'.date('YmdHis', $sec).".$u_sec", $contents);
-
+require_once '../register.log.inc';
 require_once '../../panel/class/Conn.class.php';
 require_once '../../panel/class/Callback.class.php';
+
+register_gateway_log();
 
 $request = $_REQUEST;
 $callback = new Callback($request,'asaas');
