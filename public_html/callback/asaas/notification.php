3c2d0162d142572e7ac7c6d99bc537582c26fa46
rdteixeira <rodrigo@computatus.org>
2023-08-19 13:46:58 -0300

    Alterações - Callback/Asaas - Reorganização de Codigo

--- public_html/callback/asaas/notification.php
+++ public_html/callback/asaas/notification.php
@@ -1,17 +1,16 @@
-<?php 
+<?php
 
- $server = (object) $_SERVER;
- $contents = "$server->REQUEST_METHOD $server->REQUEST_URI $server->SERVER_PROTOCOL\n";
- foreach(getallheaders() as $key => $value) $contents .= "$key: $value\n";
- $payload = file_get_contents('php://input');
- $contents .= "\n".$payload;
- file_put_contents('request-'.microtime(true), $contents);
+$server = (object) $_SERVER;
+$contents = "$server->REQUEST_METHOD $server->REQUEST_URI $server->SERVER_PROTOCOL\n";
+foreach(getallheaders() as $key => $value) $contents .= "$key: $value\n";
+$payload = file_get_contents('php://input');
+$contents .= "\n".$payload;
+list($sec, $u_sec) = explode('.', microtime(true));
+file_put_contents('request-'.date('YmdHis', $sec).".$u_sec", $contents);
 
- require_once '../../panel/class/Conn.class.php';
- require_once '../../panel/class/Callback.class.php';
+require_once '../../panel/class/Conn.class.php';
+require_once '../../panel/class/Callback.class.php';
 
- $request = $_REQUEST;
- $callback = new Callback($request,'asaas');
- $callback->callback();
-
-?>
+$request = $_REQUEST;
+$callback = new Callback($request,'asaas');
+$callback->callback();
