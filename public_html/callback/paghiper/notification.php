42ca01f03ef5752f597b9ff2ab609be2a4d25aaa
rdteixeira <rodrigo@computatus.org>
2023-08-19 14:17:31 -0300

    Alterações - Callback/PagHiperMercadoPago - Reorganização de Codigo

--- public_html/callback/paghiper/notification.php
+++ public_html/callback/paghiper/notification.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 
- require_once '../../panel/class/Conn.class.php';
- require_once '../../panel/class/Callback.class.php';
+require_once '../register.log.inc';
+require_once '../../panel/class/Conn.class.php';
+require_once '../../panel/class/Callback.class.php';
 
- $request = $_REQUEST;
- $callback = new Callback($request,'paghiper');
- $callback->callback();
+register_gateway_log();
 
-?>
+$request = $_REQUEST;
+$callback = new Callback($request,'paghiper');
+$callback->callback();
